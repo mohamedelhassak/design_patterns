0cc3a171d934358768247501d0c30598d2cdfd16
Elhassak mohamed <mohamed.elhassak2@gmail.com>
2020-12-14 18:05:48 +0100

    add protoype2

---
+++ src/DP_Prototype2/Circle.java
@@ -0,0 +1,11 @@
+package DP_Prototype2;
+
+public class Circle extends Shape {
+    public Circle(){
+        type = "Circle";
+    }
+
+    @Override public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
---
+++ src/DP_Prototype2/Rectangle.java
@@ -0,0 +1,11 @@
+package DP_Prototype2;
+
+public class Rectangle extends Shape {
+    public Rectangle(){
+        type = "Rectangle";
+    }
+
+    @Override public void draw() {
+        System.out.println("Inside Rectangle::draw() method.");
+    }
+}
